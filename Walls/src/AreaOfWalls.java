daab89ac183d7837de288a9f0ad74c23ea19fdab
Patrick <Patrickmitchell59@gmail.com>
2021-06-14 19:30:20 +0100

    initial commit

---
+++ Walls/src/AreaOfWalls.java
@@ -0,0 +1,12 @@
+public class AreaOfWalls {
+    public static void main(String[] args) {
+        String HeightOfWall = "Height of the wall";
+        String WidthOfWall = "Width of the wall";
+          String WallWidth = "100";
+           String WallHeight = "50";
+        int Width = Integer.parseInt(WallWidth);
+        int Height = Integer.parseInt(WallHeight);
+        System.out.print("The amount of paint required is" + " " + Height + " " + "*" + " " + Width + "=" + Height*Width + " " + "In" + " "+ "square metres, then combined for each room");
+
+    }
+}
