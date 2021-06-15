dec9bad8c016c5fac2df53274b34ba76d55669a0
pmitch96 <85648033+pmitch96@users.noreply.github.com>
2021-06-15 11:24:04 +0100

    Added in obstructions and some comments

--- Walls/src/AreaOfWalls.java
+++ Walls/src/AreaOfWalls.java
@@ -7,24 +7,25 @@ public class AreaOfWalls {
       double Wall_width = Input_Width.nextDouble();
       Scanner Input_Height = new Scanner(System.in);
       System.out.println("enter the Height of the wall, numerically in metres");
-      double Wall_length = Input_Height.nextDouble();
-      double Wall_area = Wall_width*Wall_length;
-      System.out.println("Area of Rectangle is:"+Wall_area);
+      double Wall_height = Input_Height.nextDouble();
+      Scanner Input_area_of_obstructions = new Scanner(System.in);
+      System.out.println("enter the area of any obstructions, such as windows or doors, numerically in metres");
+      double Obstruction_area = Input_area_of_obstructions.nextDouble();
+      double Wall_area =(Wall_width*Wall_height) - Obstruction_area;
 //change height to breadth, as the ceiling won't have a height
-          String WallWidth = "100";
-          String WallHeight = "50";
-        int Width = Integer.parseInt(WallWidth);
-        int Height = Integer.parseInt(WallHeight);
-        int area = Width * Height;
-        System.out.println(area);
-        if (Width == 100) {
+        // need to do other walls and the ceiling
+        // need to account for windows etc.
+        // need to add in cost and coats of paint
+        // use mm instead of m?
+        if (Wall_height >= 10) {
             System.out.println("Why do you have such a ridiculously large wall you maniac");
         }
         else {
             System.out.println("The height of the wall is not 100 metres");
         }
-        System.out.println("The amount of paint required is " + area + " square metres, then combined for each wall");
-      switch (area) {
+        System.out.println("The amount of paint required is " + Wall_area + " square metres, then combined for each wall");
+        int WallArea = (int) Math.round(Wall_area);
+      switch (WallArea) {
           case 5000:
           System.out.println("We're gonna need a stupid amount of paint");
           break;
