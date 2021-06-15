87de6d6630bda267766ad9b55eaa2907e210ab8c
pmitch96 <85648033+pmitch96@users.noreply.github.com>
2021-06-15 10:21:10 +0100

    Added in if statement and switch

--- Walls/src/AreaOfWalls.java
+++ Walls/src/AreaOfWalls.java
@@ -1,12 +1,34 @@
+// import java.util.Scanner;
+
 public class AreaOfWalls {
     public static void main(String[] args) {
-        String HeightOfWall = "Height of the wall";
-        String WidthOfWall = "Width of the wall";
+      // Scanner s= new Scanner(System.in);
+      // System.out.println("enter the length of the wall, numerically in metres")
+// double
+
           String WallWidth = "100";
-           String WallHeight = "50";
+          String WallHeight = "50";
         int Width = Integer.parseInt(WallWidth);
         int Height = Integer.parseInt(WallHeight);
-        System.out.print("The amount of paint required is" + " " + Height + " " + "*" + " " + Width + "=" + Height*Width + " " + "In" + " "+ "square metres, then combined for each room");
+        int area = Width * Height;
+        System.out.println(area);
+        if (Width == 100) {
+            System.out.println("The height of the wall is 100 metres");
+        }
+        else {
+            System.out.println("The height of the wall is not 100 metres");
+        }
+        System.out.println("The amount of paint required is " + area + " square metres, then combined for each wall");
+      switch (area) {
 
-    }
-}
+          case 5000:
+          System.out.println("We're gonna need a stupid amount of paint");
+          break;
+          case 1:
+          System.out.println("We don't need as much paint");
+          break;
+
+
+      }
+    }}
+
